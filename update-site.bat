95e56f50caed57ba011d446fe9e89af6a7f4c7ae
Ivan ComputerMaster <ivanhoron@mail.ru>
2025-10-11 04:04:34 +0300

    Full update: design, images, multilanguage

--- update-site.bat
+++
@@ -1,8 +0,0 @@
-@echo on
-echo Updating site for Vercel (vercel-version)...
-cd /d "%~dp0\vercel-version"
-git add --all
-git commit -m "Update site (visual & multilingual)"
-git push origin main
-echo Done.
-pause
